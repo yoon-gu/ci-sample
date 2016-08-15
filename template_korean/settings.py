0b93fa1e465def0eeaa1319877508bdd4d139a8d
Yoonku Hwang <yz0624@gmail.com>
2016-08-15 15:23:17 +0900

    ./manage.py test

--- template_korean/settings.py
+++ template_korean/settings.py
@@ -76,3 +76,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_URL = '/static/'
