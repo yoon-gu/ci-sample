b3e423bf3288e495dd988da8f4638942b9af7e6f
Yoonku Hwang <yz0624@gmail.com>
2016-08-15 15:37:54 +0900

    add test

--- template_korean/tests.py
+++ template_korean/tests.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django.test import TestCase
 
 # Create your tests here.
@@ -6,3 +7,11 @@ class TemplateKoreanTestCase(TestCase):
 		pass
 	def test_test(self):
 		self.assertEqual(1, 1)
+	def test_korean_proofread(self):
+		import korean
+		res = korean.l10n.proofread(u"황윤구(은)는 지금 졸리다.")
+		self.assertEqual(res, u"황윤구는 지금 졸리다.")
+	def test_korean_proofread_fail(self):
+		import korean
+		res = korean.l10n.proofread(u"황윤구(은)는 지금 졸리다.")
+		self.assertNotEqual(res, u"황윤구은 지금 졸리다.")
