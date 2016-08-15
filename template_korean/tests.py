c3effba3e01e6ec0c9555f5e3e0ad988cadb4ddc
Yoonku Hwang <yz0624@gmail.com>
2016-08-15 16:18:25 +0900

    success

--- template_korean/tests.py
+++ template_korean/tests.py
@@ -5,13 +5,23 @@ from django.test import TestCase
 class TemplateKoreanTestCase(TestCase):
 	def setUp(self):
 		pass
+	
 	def test_test(self):
 		self.assertEqual(1, 1)
+	
 	def test_korean_proofread(self):
 		import korean
 		res = korean.l10n.proofread(u"황윤구(은)는 지금 졸리다.")
 		self.assertEqual(res, u"황윤구는 지금 졸리다.")
+	
 	def test_korean_proofread_fail(self):
 		import korean
 		res = korean.l10n.proofread(u"황윤구(은)는 지금 졸리다.")
 		self.assertNotEqual(res, u"황윤구은 지금 졸리다.")
+
+	def test_korean_proofread_tag(self):
+		from django.template import Context, Template
+		rendered = Template(
+			'{% load proofread %}'
+			'{{ string|proofread }}').render(Context({'string':u'황윤구(은)는 지금 졸리다.'}))
+		self.assertEqual(rendered, u'황윤구는 지금 졸리다.')
